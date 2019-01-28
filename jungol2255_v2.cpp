46cb367d002a4e4c8696633f6c2e0f7e253c4905
YJ15 <whfod2@hanmail.net>
2019-01-28 09:40:53 +0900

    섞기수열 풀이 개선
    1. lcm 계산을 궤적탐색 for문 안으로 이동, find 대신 tracepairts 로 한 번 계산했던 숫자를 lcm 연산 안 하도록 변경.
    2. visit를 사용하여 한 번 탐색한 궤적은 재탐색 안하도록 함. (1->3->5->1 이나 3->5->1->3 이나 같은 순서&횟수로 탐색을 하기 때문)
    위의 개선을 통해 1000~1200 ms 걸리던 속도가 10~12 ms 으로 줄어들었고, TLE였던 14번까지 모두 성공함.

---
+++ jungol2255_v2.cpp
@@ -0,0 +1,88 @@
+/**************************************************************
+Problem: jungol2255
+User: emsw2013
+Language: C++
+Result: Time Limit Exceed(93)
+Time:10 ms
+Memory:1 MB
+****************************************************************/
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+long gcd(long a, long b)
+{
+	return b ? gcd(b, a%b) : a;
+}
+
+long lcm(long a, long b)
+{
+	return a / gcd(a, b)*b;
+}
+
+int main()
+{
+	// 1. get input
+	int N;										// 카드 개수 (1~20,000)
+	vector<int> progression;			// 수열 
+	vector<bool> visit;					// true: 탐색한 궤적 (탐색한 궤적 중복연산 방지)
+	map<int, bool> tracePairs;		// key: 카드 별 궤적 (같은 궤적 lcm 중복연산 방지)
+	int lcmTrace = 1;						// 수열의 최종 궤적 = return 
+	int count = 0;							// 카운팅 할 때 사용 (1 ~ 2,000,000,000)
+
+	cin >> N;
+	visit.reserve(N);
+	progression.resize(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> progression[i];
+		progression[i]--;
+
+		if (progression[i] == i)
+		{
+			count++;
+		}
+	}
+
+	// 2. search
+	if (count == N) // 카드 위치와 수열이 모두 일치한 입력 거르기
+	{
+		cout << 0;
+		return 0;
+	}
+	else
+	{
+		count = 0;
+		for (int i = 0; i < N; i++) // card 1, ..., N
+		{
+			int idx = i;
+			count = 0;
+
+			if (progression[i] != i)  // 카드 번호와 수열이 일치한 경우 궤적이 0이므로 거름
+			{
+				do
+				{
+					idx = progression[idx]; 
+					count++;
+					visit[idx] = true;
+					if (visit[progression[idx]]) break; 
+				} while (idx != i);
+
+				// lcm
+				if (tracePairs[count] == false) 
+				{
+					lcmTrace = lcm(lcmTrace, count); 
+				}
+				tracePairs[count] = true;
+			}
+		}
+	}
+
+	cout << lcmTrace; // 1 ~ 2,000,000,000
+
+	return 0;
+}
